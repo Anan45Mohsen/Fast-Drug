71375413ad79477de50975985825a9414a2084a5
Anan  Mohsen <99495940+Anan45Mohsen@users.noreply.github.com>
2023-07-07 12:25:33 +0300

    Update register.js
    Update register.js

--- routers/register.js
+++ routers/register.js
@@ -16,11 +16,11 @@ router.get("/register/:userId", auth,(req, res) => {
   registerController.getUserById(req, res)
 })
 
-router.put("/register/:id", auth,checkRole(USER_ROLE , ASSOCIATION_ROLE),async (req, res) => {
+router.put("/register/:id", auth,checkRole(ASSOCIATION_ROLE,USER_ROLE),async (req, res) => {
   await registerController.updateUserInfoById(req, res)
 })
 
-router.delete("/register/:userId", auth,checkRole(USER_ROLE , ASSOCIATION_ROLE),(req, res) => {
+router.delete("/register/:userId", auth,checkRole(ASSOCIATION_ROLE,USER_ROLE),(req, res) => {
   registerController.deleteUserById(req, res)
 })
 
